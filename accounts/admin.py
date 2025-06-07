a6b2c5ff4024b9c258bc148171b179c085a85ae9
sesco001 <marcasmatheka@gmail.com>
2025-06-07 17:50:58 +0300

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,24 @@
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from .forms import CustomUserCreationForm
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    model = CustomUser
+    list_display = ('phone_number', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('phone_number', 'email')
+    ordering = ('phone_number',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
